4ba2a0a23fb81d1704a35aa5c983dfaa5099338d
SWIFT\Vox <voxatrophia@gmail.com>
2017-01-21 00:31:31 -0800

    Friday Night - Basic movement. Sonar prototype. Added some seafloor textures.

---
+++ Sonar Stealth (1)/Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float maxSpeed;
+    Rigidbody subRB;
+
+	// Use this for initialization
+	void Start () {
+        subRB = GetComponent<Rigidbody>();   	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        ApplyTorqueAndForce();
+	}
+
+    void ApplyTorqueAndForce()
+    {
+        float turnRight = Input.GetAxis("LeftScrew") * maxSpeed * Time.deltaTime;
+        float turnLeft = Input.GetAxis("RightScrew") * maxSpeed * Time.deltaTime;
+
+        subRB.AddTorque(transform.up * (turnRight-turnLeft));
+        subRB.AddForce(transform.forward * (turnRight + turnLeft));
+    }
+}
---
+++ Sonar Stealth (1)/Assets/Scripts/SonarPing.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class SonarPing : MonoBehaviour {
+
+    public float maxBrightness;
+    public float minBrightness;
+    public float onDelay;
+    public float offDelay;
+    public float transitionTime;
+
+    Light lt;
+
+    void Awake() {
+        lt = GetComponent<Light>();
+        lt.range = 0;
+    }
+
+    void Start() {
+        StartCoroutine(Transition(true));
+    }
+
+    IEnumerator Transition(bool turnOn)
+    {
+        float initialBrightness = lt.range;
+        float targetBrightness = turnOn ? maxBrightness : minBrightness;
+        float startTime = Time.time;
+        float endTime = startTime + transitionTime;
+
+        while (endTime >= Time.time)
+        {
+            lt.range = Mathf.Lerp(initialBrightness, targetBrightness, (Time.time - startTime) / transitionTime);
+            yield return null;
+        }
+
+        if (turnOn)
+        {
+            yield return new WaitForSeconds(onDelay);
+        }
+        else {
+            yield return new WaitForSeconds(offDelay);
+        }
+
+
+        StartCoroutine(Transition(!turnOn));
+    }
+
+}
